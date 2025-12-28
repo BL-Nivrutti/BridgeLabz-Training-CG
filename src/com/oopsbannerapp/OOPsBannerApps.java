fda90056baf1c8e97878df3a3dedcb941ff6e19c
nivruttigithub <nivruttiwagh2002@gmail.com>
2025-12-28 23:05:30 +0530

    [Nivrutti] feat(UC2): render OOPS as banner using spaces and asterisks

--- src/com/oopsbannerapp/OOPsBannerApps.java
+++ src/com/oopsbannerapp/OOPsBannerApps.java
@@ -17,6 +17,50 @@ public class OOPsBannerApps {
 public static void main(String[] args) {
 	
 	System.out.println("OOPS");
+	// 7 × 9 block-style letters
+    String[] O = {
+            "  *****  ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "  *****  "
+    };
+
+    String[] P = {
+            " ******  ",
+            " **    ** ",
+            " **    ** ",
+            " ******  ",
+            " **       ",
+            " **       ",
+            " **       "
+    };
+
+    String[] S = {
+            "  *****  ",
+            " **       ",
+            " **       ",
+            "  *****  ",
+            "      **  ",
+            "      **  ",
+            "  *****  "
+    };
+
+
+    // Final banner rows
+    String[] banner = new String[7];
+
+    // Build banner line by line
+    for (int i = 0; i < 7; i++) {
+        banner[i] = String.join("  ", O[i], O[i], P[i], S[i]);
+    }
+
+    // Print banner
+    for (String line : banner) {
+        System.out.println(line);
+    }
 }
 	
 }
