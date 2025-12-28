b245c7e923d158ab47f668488d8a6e68dfe10039
nivruttigithub <nivruttiwagh2002@gmail.com>
2025-12-28 22:59:00 +0530

    [Nivrutti] feat(UC1): print OOPS to console using the basic java program

---
+++ src/com/oopsbannerapp/OOPsBannerApps.java
@@ -0,0 +1,22 @@
+package com.oopsbannerapp;
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application
+ * that prints the OOPS acronym to the console.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class OOPsBannerApps {
+	 /**
+     * Entry point of the Java application.
+     *
+     * @param args command-line arguments
+     */
+public static void main(String[] args) {
+	
+	System.out.println("OOPS");
+}
+	
+}
